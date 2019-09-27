6362da3d15b904c5be18540b979a717316630af8
Christian <christianleyva@vilop.com>
2019-09-26 21:20:20 -0400

    devuelve

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+require("./user");
+const routes = require('./routes');
+
+
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost:27017/registrations", { useNewUrlParser: true,useUnifiedTopology: true });
 
 const app = express();
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/products', { useNewUrlParser: true });
+app.set('view engine', 'pug');
+app.set('views', 'views');
+app.set('trust proxy', 1) // trust first proxy
 
-const ProductSchema = new mongoose.Schema({
-  name: String,
-  price: Number
-});
-const Product = mongoose.model("Product", ProductSchema);
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/products", async (req, res) => {
-  const products = await Product.find();
-  res.json(products);
-});
+app.use(cookieSession({
+    secret:"buenasperrito",
+    maxAge: 24*60*60*1000
+}));
+
+app.use('/', routes);
+
+
 
 app.listen(3000, () => console.log("Listening on port 3000 ..."));
